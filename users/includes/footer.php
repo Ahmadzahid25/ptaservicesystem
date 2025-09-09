f59dd9ffb75579f39075c979e4cfca23a9d0253f
Ahmad Zahid <128762005+Ahmadzahid25@users.noreply.github.com>
2025-09-09 17:41:47 +0800

    Update footer.php

--- users/includes/footer.php
+++ users/includes/footer.php
@@ -1,6 +1,6 @@
  <footer class="site-footer">
           <div class="text-center">
-              <?php echo date('Y')?> - ZAHIDXNUMAN.IO - copyright
+              <?php echo date('Y')?> - 2025 DFKIKMBesut. All rights reserved.
               <a href="#" class="go-top">
                   <i class="fa fa-angle-up"></i>
               </a>
